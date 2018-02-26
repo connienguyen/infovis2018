4a87f3bab7f9eacee2b98939e66eecc7ed1b2cae
Daniel Roeven <daniel@roeven.com>
2018-02-26 09:58:18 +0100

    Zoom and year slider.

--- vis.js
+++ vis.js
@@ -1,9 +1,9 @@
 var zoomSlider;
-var bubbleReducer = 110;
+var yearSlider;
 
 document.addEventListener('DOMContentLoaded', function(){
 
-  
+
   var educationCSV = "data/education.csv";
   var employmentCSV = "data/employment_only_job.csv";
   var demographicsCSV = "data/demographics.csv";
@@ -22,6 +22,7 @@ document.addEventListener('DOMContentLoaded', function(){
   });
 
   zoomSlider = document.getElementById('zoomSlider');
+  yearSlider = document.getElementById('yearSlider');
 });
 
 const createDemographicsBubbleChart = function(demographics){
@@ -42,18 +43,28 @@ const createDemographicsBubbleChart = function(demographics){
   var largestCountries = _.take(countriesSortedBySize, 10);
   var range = ages.length;
   var domain = largestCountries.length;
-  const getZoomFactor = function(sliderValue) { return 32 / (largestCountries[0].total * 0.005) * sliderValue; };
+  const getZoomFactor = function(sliderValue) { return 64 / (largestCountries[0].total * 0.005) * sliderValue; };
   var zoom = getZoomFactor(zoomSlider.value);
+  var year = yearSlider.value;
   var height = 700 - 16;
   var width = 1024;
   var svg = d3.select('svg');
 
   zoomSlider.addEventListener('input', function(e) {
     console.log(e.target.value)
- zoom = getZoomFactor(e.target.value);
+    zoom = getZoomFactor(e.target.value);
     largestCountries.forEach(function(country){
-      var bubblesForCountry = svg.selectAll('.' + country.name.substring(0,5)) // Substring is a hack to avoid classname with spaces
-        .attr('r', function(row) { return zoom * Math.sqrt(row[e.target.value]/bubbleReducer) })
+      var bubblesForCountry = svg.selectAll('.' + country.name.substring(0,5)) // Substring=hack to avoid classname with spaces
+        .attr('r', function(row) { return zoom * Math.sqrt(row[year]) })
+    });
+  });
+
+  yearSlider.addEventListener('input', function(e) {
+    console.log(e.target.value)
+    year = e.target.value;
+    largestCountries.forEach(function(country){
+      var bubblesForCountry = svg.selectAll('.' + country.name.substring(0,5)) // Substring=hack to avoid classname with spaces
+        .attr('r', function(row) { return zoom * Math.sqrt(row[year]) })
     });
   });
 
@@ -63,10 +74,10 @@ const createDemographicsBubbleChart = function(demographics){
     var circles = svg.selectAll('circle')
             .data(country, function(row) { return row['2016']; })
           .enter().append('circle')
-            .attr('class', countryName.substring(0,5)) // Substring is a hack to avoid classname with spaces
+            .attr('class', countryName.substring(0,5)) // Substring=hack to avoid classname with spaces
             .attr('fill', 'steelblue')
             .attr('opacity', (countryName == 'Sverige' ? .1 : 1)) // Change opacity
-            .attr('r', function(row) { return zoom * Math.sqrt(row[zoomSlider.value]/bubbleReducer) })
+            .attr('r', function(row) { return zoom * Math.sqrt(row[year]) })
             .attr('cx', function(row) { return 128 + ((width - 128) / domain * rank) + ((width - 128) / domain / 2); })
             .attr('cy', function(row, index) { return height - ((height / range) * index) - (height / range / 2); });
   };
